0f16717cb7eb3c134813670aebd8191d5ac6eda6
Jordi Mas <jmas@softcatala.org>
2023-04-21 07:19:02 +0200

    Fix

--- faster-whisper/inference.py
+++ faster-whisper/inference.py
@@ -1,18 +1,13 @@
 from faster_whisper import WhisperModel
 
-model_size = "medium"
-compute_type = "float32"
-model = WhisperModel(model_size, device="cpu", compute_type=compute_type,  cpu_threads=14)
+model = WhisperModel("small", device="auto")
 
-print(f"model: {model_size}, compute_type {compute_type}")
+segments, info = model.transcribe("file.mp3")
 
-for i in range(0,3):
-    segments, info = model.transcribe("15GdH9-curt.mp3")
+print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
-    print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
-
-    for segment in segments:
-        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
-        if segment.end > 60:
-            break
+for segment in segments:
+    print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+    if segment.end > 60:
+        break
 
