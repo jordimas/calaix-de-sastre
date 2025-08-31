d20caf67345a4576f5317365fe2c9aa5495e2f2f
Jordi Mas <jmas@softcatala.org>
2025-08-31 10:30:02 +0200

    More

--- eval_translation/run_all.sh
+++ eval_translation/run_all.sh
@@ -1,4 +1,9 @@
 sudo pkill -9 python
 sudo pkill -9 python3
 cd dataset && python generate.py && cd ..
-python reviewer.py --prompt_version 3
+
+for i in 1 2 2_1 3; do
+    python reviewer.py --prompt_version "$i"
+done
+
+
